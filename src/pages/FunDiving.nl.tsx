8ae28ca88490ea58ad8b1e49a2d62254633409bb
one-media-asia <onemediaasia@duck.com>
2026-05-07 05:53:23 +0800

    x

--- src/pages/FunDiving.nl.tsx
+++ src/pages/FunDiving.nl.tsx
@@ -801,7 +801,7 @@ const FunDiving = () => {
                 </div>
                 <Button
                   size="lg"
-                  onClick={() => window.open('/booknow.html', '_blank')}
+                  onClick={() => window.open('/bookingform.html', '_blank')}
                 >
                   Verstuur boekingsaanvraag
                 </Button>
